ece3474374fe1b0569bd11db16dd689b60cac469
Victor <victorsubota@gmail.com>
2017-12-06 23:36:35 +0200

    Add message history. There are too many bags..!

---
+++ src/main/java/com/viteksu/kursach/core/messageSystem/messages/userDataService/userMessage/AddingMessageMsg.java
@@ -0,0 +1,32 @@
+package com.viteksu.kursach.core.messageSystem.messages.userDataService.userMessage;
+
+import com.viteksu.kursach.core.messageSystem.Abonent;
+import com.viteksu.kursach.core.messageSystem.addressService.Address;
+import com.viteksu.kursach.web.backEnd.accounts.UserDataService;
+import com.viteksu.kursach.web.backEnd.accounts.Message;
+
+import java.util.List;
+
+public class AddingMessageMsg extends com.viteksu.kursach.core.messageSystem.messages.Message {
+    private List<Message> messages;
+
+    public AddingMessageMsg(Address from, Address to, List<Message> messages) {
+        super(from, to);
+        this.messages = messages;
+    }
+
+    @Override
+    public void execute(Abonent abonent) {
+        if (abonent instanceof UserDataService)
+            execute((UserDataService) abonent);
+
+    }
+
+    public void execute(UserDataService userDataService) {
+        userDataService.addMessages(messages);
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+}
